51e763656eafecc4aa7e79aaef01ebdfa5ff3c6b
buzolin3 <130096669+buzolin3@users.noreply.github.com>
2024-05-21 17:38:51 -0300

    TyerMan 0.0.5

--- script.js
+++ script.js
@@ -5,9 +5,60 @@ var numero = 0;
 var totalElement;
 var camisetaPremium = document.querySelectorAll("#Premium");
 var camisetaSurf = document.querySelectorAll("#Surf");
-var entrega = document.querySelectorAll(".entrega");
-var retirada =document.querySelectorAll(".retirada");
 var carrinho = [];
+var opcaoEntrega;
+var entregaRadio = document.getElementById("entregaRadio");
+var retiradaRadio = document.getElementById("retiradaRadio");
+var dinheiroRadio = document.getElementById("flexRadioDefault1");
+var cartaoRadio = document.getElementById("flexRadioDefault2");
+var pixRadio = document.getElementById("flexRadioDefault3");
+var troco = document.getElementById("troco");
+
+document.addEventListener('DOMContentLoaded', function () {
+    var entrega = document.getElementById("entrega");
+    var retirada = document.getElementById("retirada");
+
+    entregaRadio.addEventListener('change', function () {
+        if (this.checked) {
+            opcaoEntrega = "Entrega: ";
+            entrega.classList.add('d-block');
+            entrega.classList.remove('d-none');
+            retirada.classList.remove('d-block');
+            retirada.classList.add('d-none');
+        }
+    });
+
+    retiradaRadio.addEventListener('change', function () {
+        if (this.checked) {
+            opcaoEntrega = "Retirada: ";
+            entrega.classList.add('d-none');
+            entrega.classList.remove('d-block');
+            retirada.classList.remove('d-none');
+            retirada.classList.add('d-block');
+        }
+    });
+
+    dinheiroRadio.addEventListener('change',function () {
+        if(this.checked){
+            troco.classList.remove('d-none');
+            troco.classList.add('d-block');
+        }
+    })
+
+    cartaoRadio.addEventListener('change',function () {
+        if(this.checked){
+            troco.classList.add('d-none');
+            troco.classList.remove('d-block');
+        }
+    })
+
+    pixRadio.addEventListener('change',function () {
+        if(this.checked){
+            troco.classList.add('d-none');
+            troco.classList.remove('d-block');
+        }
+    })
+});
 
 
 //Coloca classe no media query
@@ -34,29 +85,6 @@ window.addEventListener('load', checkWindowSize);
 // Verifica o tamanho da janela ao redimensionar a janela
 window.addEventListener('resize', checkWindowSize);
 
-
-//mostrar Entrega
-function mostrarEntrega(){
-    retirada.forEach(function(retirada){
-        retirada.classList.add('d-none');
-    })
-    entrega.forEach(function(entrega){
-        entrega.classList.remove('d-none');
-        entrega.classList.add('d-block');
-    })
-}
-
-//mostrar Retirada
-function mostrarRetirada(){
-    entrega.forEach(function(entrega){
-        entrega.classList.add('d-none');
-    })
-    retirada.forEach(function(retirada){
-        retirada.classList.remove('d-none');
-        retirada.classList.add('d-block');
-    })
-}
-
 //Mostrar todos produtos
 function mostrarTodos() {
     camisetaPremium.forEach(function (camisetaPremium) {
@@ -68,6 +96,7 @@ function mostrarTodos() {
         camisetaSurf.classList.add('d-block');
     });
 }
+
 
 //Mostrar as Surfs na tela
 function mostrarSurf() {
@@ -119,7 +148,7 @@ function adicionarCarrinho(nomeProduto, valorProduto) {
 
     if (selectedColor && selectedTamanho) {
         mensagemProduto.innerHTML = "1 - " + nomeProduto + "<br>" + " Cor: " + selectedColor + "<br>" + " Tamanho: " + selectedTamanho;
-        carrinho.push("%0a" + nomeProduto);
+        carrinho.push("%0a" + nomeProduto + " " + selectedColor + " " + selectedTamanho);
         produtosAdicionados.appendChild(mensagemProduto);
         numero++;
         (document.getElementById("numero-carrinho").innerHTML = numero);
@@ -148,3 +177,72 @@ function adicionarCarrinho(nomeProduto, valorProduto) {
     return 0;
 }
 
+
+// Função enviar Pedido para Whatsapp
+function enviarPedido() {
+    var nome = document.getElementById("nome").value;
+    var celular = document.getElementById("celular").value;
+    var email = document.getElementById("email").value;
+    var cpf = document.getElementById("cpf").value;
+    var opcaoPagamento;
+    var cidade = document.getElementById("cidade").value
+    var bairro = document.getElementById("bairro").value;
+    var rua = document.getElementById("rua").value;
+    var numeroCasa = document.getElementById("numeroCasa").value;
+    var complemento = document.getElementById("complemento").value;
+
+    //Verifica se escolheu cartão ou dinheiro e o troco
+    if (cartaoRadio.checked) {
+        opcaoPagamento = "Cartão "
+    } else if (dinheiroRadio.checked) {
+        opcaoPagamento = "Dinheiro " + "%0a" + "%0a" + "Troco para: " + troco.value;
+    } else if (pixRadio.checked) {
+        opcaoPagamento = "Pix "
+    }
+
+    //API para enviar para Whatsapp
+    var linkEntrega = "https://api.whatsapp.com/send/?phone=16997696990&text=" +
+        "Pedido" + "%0a" +
+        "%0a" + "Nome do Cliente: " + nome + "%0a" +
+        "%0a" + "Celular: " + celular + "%0a" +
+        "%0a" + "Email: " + email + "%0a" +
+        "%0a" + "CPF: " + cpf + "%0a" +
+        "%0a" + "Produtos Selecionados: " + carrinho + "%0a" +
+        "%0a" + "Valor a pagar: " + totalCompra + "%0a" +
+        "%0a" + "Opção Pagamento: " + opcaoPagamento + "%0a" +
+        "%0a" + "Opção Entrega: " + opcaoEntrega + "%0a" +
+        "%0a" + "Cidade: " + cidade + "%0a" +
+        "%0a" + "Bairro: " + bairro + "%0a" +
+        "%0a" + "Rua: " + rua + "%0a" +
+        "%0a" + "Numero Casa: " + numeroCasa + "%0a" +
+        "%0a" + "Complemento: " + complemento; "%0a";
+
+    var linkRetirada = "https://api.whatsapp.com/send/?phone=16997696990&text=" +
+        "Pedido" + "%0a" +
+        "%0a" + "Nome do Cliente: " + nome + "%0a" +
+        "%0a" + "Celular: " + celular + "%0a" +
+        "%0a" + "Email: " + email + "%0a" +
+        "%0a" + "CPF: " + cpf + "%0a" +
+        "%0a" + "Produtos Selecionados: " + carrinho + "%0a" +
+        "%0a" + "Valor a pagar: " + totalCompra + "%0a" +
+        "%0a" + "Opção Pagamento: " + opcaoPagamento + "%0a" +
+        "%0a" + "Opção Entrega: " + opcaoEntrega + "%0a";
+
+
+
+    //Verificando se todos campos obrigatorios foram preenchidos
+    
+    if(!entregaRadio.checked && !retiradaRadio.checked){
+        alert("Selecione uma opção de entrega");
+    } else if(nome.trim() === "" || celular.trim() === "" || email.trim() === "" || cpf.trim() === ""){
+        alert("Por favor, preencha todos os campos obrigatórios.")
+    } else if(entregaRadio.checked){
+        if(cidade.trim() === "" || bairro.trim() === "" || rua.trim() === "" || numeroCasa.trim() === ""){
+            alert("Por favor, preencha todos os campos obrigatórios.");
+        } else{
+            window.location.href = linkEntrega;
+        }
+    } else if(retiradaRadio.checked){
+        window.location.href = linkRetirada;
+    }
+}
